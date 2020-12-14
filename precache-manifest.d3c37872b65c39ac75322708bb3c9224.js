4feed35f90b3dcc2f804a8491e55d2b7f90c94e4
Ashutosh <akashpatale88@gmail.com>
2020-12-14 21:55:25 +0530

    Updates

---
+++ precache-manifest.d3c37872b65c39ac75322708bb3c9224.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c25706224c75e0f429758a774df56a98",
+    "url": "/AshuPortfoliowebsite/index.html"
+  },
+  {
+    "revision": "99b8bdfd0b7eac11ad26",
+    "url": "/AshuPortfoliowebsite/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "23f5f09375b19ab58160",
+    "url": "/AshuPortfoliowebsite/static/css/main.470e4ff8.chunk.css"
+  },
+  {
+    "revision": "99b8bdfd0b7eac11ad26",
+    "url": "/AshuPortfoliowebsite/static/js/2.e7488496.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/AshuPortfoliowebsite/static/js/2.e7488496.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23f5f09375b19ab58160",
+    "url": "/AshuPortfoliowebsite/static/js/main.872ed905.chunk.js"
+  },
+  {
+    "revision": "14ed202f3fc33988bbae",
+    "url": "/AshuPortfoliowebsite/static/js/runtime-main.d23e642e.js"
+  }
+]);
